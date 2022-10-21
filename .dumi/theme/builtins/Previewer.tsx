be9d7a5b4ee532ddd089640daae9d2907fdd18f6
wangyes <wangyes1996@163.com>
2022-10-21 15:03:21 +0800

    初始化

---
+++ .dumi/theme/builtins/Previewer.tsx
@@ -0,0 +1,40 @@
+﻿// @ts-ignore
+import PreView, { IPreviewerProps } from 'dumi-theme-default/src/builtins/Previewer';
+import LazyLoad from 'react-lazyload';
+export default ({
+  children,
+  ...rest
+}: IPreviewerProps & {
+  height: string;
+  iframe: string;
+}) => {
+  if (process.env.NODE_ENV === 'production') {
+    return (
+      <PreView {...rest}>
+        <div
+          style={{
+            contentVisibility: 'auto',
+            contain: 'style layout paint',
+          }}
+        >
+          {children}
+        </div>
+      </PreView>
+    );
+  }
+
+  return (
+    <PreView {...rest}>
+      <LazyLoad once offset={500}>
+        <div
+          style={{
+            contentVisibility: 'auto',
+            contain: 'style layout paint',
+          }}
+        >
+          {children}
+        </div>
+      </LazyLoad>
+    </PreView>
+  );
+};
---
+++ packages/components/src/Button/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Button } from 'antd';
+import 'antd/dist/antd.css'
+const myButton = () => {
+  return <Button>提交</Button>;
+};
+
+export default myButton;
